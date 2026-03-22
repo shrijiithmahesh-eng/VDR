0596bdb0513bbbb28a3a1f4b35d91d6ea89c838f
Slabs_createor <249469506+shrijiithmahesh-eng@users.noreply.github.com>
2026-03-22 20:43:59 +0530

    Create SELF-KERNELCONNECTOR.c

---
+++ SSH-CONNECT/SELF-KERNELCONNECTOR.c
@@ -0,0 +1,22 @@
+void write(char *data) {
+  // 1. Point to the Log Slab
+  FILE *log_file = fopen("/IMPO/ssh/connection.log", "a");
+
+  if (log_file == NULL) {
+    print_string("[ERROR] CANNOT FIND LOG . SKIPPING LOGGING");
+    return;
+  }
+
+  // 2. Iron-Melt the data into the file
+  fprintf(log_file, "[LOG] %s\n", data);
+
+  // 3. Seal the Steel Container
+  fclose(log_file);
+  
+  print_string("[OK] DATA WRITTEN");
+}
+  // 3. Seal the Steel Container
+  fclose(log_file);
+  
+  print_string("[OK] DATA SEALED IN VASD");
+}
