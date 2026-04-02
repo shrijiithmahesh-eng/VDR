59725da949d061486669441ae53fde978efa41b3
Slabs_createor <249469506+shrijiithmahesh-eng@users.noreply.github.com>
2026-04-02 15:15:30 +0530

    Update kernel.connect.c

--- mKcLi/kernel.connect.c
+++ mKcLi/kernel.connect.c
@@ -1,1 +1,23 @@
+#include "shell.1.c"
+void write(char *data) {
+    // 1. Point to the Log Slab
+  FILE *log_file = fopen("/IMPO/SHELL/0.txt", "a");
 
+  if (log_file == NULL) {
+    print_string("[ERROR] CANNOT FIND LOG . SKIPPING LOGGING");
+    SHELL.exu();
+ }
+
+  // 2. Iron-Melt the data into the file
+  fprintf(log_file, "[LOG] %s\n", data);
+
+  // 3. Seal the Steel Container
+  fclose(log_file);
+  
+  print_string("[OK] DATA WRITTEN");
+}
+  // 3. Seal the Steel Container
+  fclose(log_file);
+  
+  print_string("[OK] DATA SEALED IN SD");
+}
